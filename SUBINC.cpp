443cf43521e8892a64ed359cd4574194f40c6f48
Shlok1409 <81183464+Shlok1409@users.noreply.github.com>
2022-04-30 01:40:57 +0530

    Add files via upload

---
+++ SUBINC.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long n;
+	    cin>>n;
+	    
+	    long long ans = 1;
+	    
+	    vector<long long> arr(n),dp(n,1);
+	    
+	    for(long long i = 0; i < n ; i++)
+	        cin>>arr[i];
+	        
+	    for(long long i = 1; i < n ; i++)
+	    {
+	        if(arr[i - 1] <= arr[i])
+	           dp[i] = dp[i] + dp[i - 1];
+	           
+	        ans = ans + dp[i];
+	    }
+	    
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
